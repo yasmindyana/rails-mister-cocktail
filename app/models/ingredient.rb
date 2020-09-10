b96d9e98ccbf65091ed7bdc2f78915983f84886f
yasmin diyana <yasmindyana@gmail.com>
2020-09-10 15:45:32 +0800

    add validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,4 @@
 class Ingredient < ApplicationRecord
+  has_many :doses
+  validates :name, uniqueness: true, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_054748) do
+ActiveRecord::Schema.define(version: 2020_09_10_060033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,22 @@ ActiveRecord::Schema.define(version: 2020_09_10_054748) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "doses", force: :cascade do |t|
+    t.string "description"
+    t.bigint "cocktail_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+  end
+
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
